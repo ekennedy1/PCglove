6652f9837be4b74c04187b0b225f51043262950c
ekennedy1 <ekennedy@tamu.edu>
2023-11-21 19:04:17 -0600

    adding downs and ups

--- PCserver.py
+++ PCserver.py
@@ -1,9 +1,10 @@
+import time
 import socketserver
-from pynput.keyboard import Controller
+from pynput.keyboard import Controller as KeyboardController, Key
 from pynput.mouse import Button, Controller as MouseController
 
-keyboard = Controller()
 mouse = MouseController()
+keyboard = KeyboardController()
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
@@ -13,18 +14,34 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             if not self.data:
                 break
             print(f"{self.client_address[0]} wrote: {self.data}")
-        
-            # Simulate a keystroke
-            if self.data == b'Hello, PC!':
-                keyboard.type('Hello, Raspberry Pi!')
             
-            # Simulate a mouse click
-            elif self.data == b'left_click':
-                mouse.click(Button.left)
+            # Simulate a mouse click down
+            elif self.data == b'left_click_down':
+                mouse.press(Button.left)
 
-            # Simulate a right mouse click
-            elif self.data == b'right_click':
-                mouse.click(Button.right)
+            # Simulate a mouse click up
+            elif self.data == b'left_click_up':
+                mouse.release(Button.left)
+
+            # Simulate a right mouse click down
+            elif self.data == b'right_click_down':
+                mouse.press(Button.right)
+
+            # Simulate a right mouse click up
+            elif self.data == b'right_click_up':
+                mouse.release(Button.right)
+            
+            # Simulate ALT + TAB with basic implementation
+            elif self.data == b'alt_tab_down':
+                keyboard.press(Key.alt)
+                keyboard.press(Key.tab)
+                keyboard.release(Key.tab)
+            
+            # Simulate ALT + TAB with basic implementation
+            elif self.data == b'alt_tab_up':
+                keyboard.press(Key.tab)
+                keyboard.release(Key.tab)
+                keyboard.release(Key.alt)
 
 if __name__ == "__main__":
     HOST, PORT = "0.0.0.0", 12345
